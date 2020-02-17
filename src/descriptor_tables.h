1896cbb6bbf0f399fff183fc3964b87aacdc0045
NothingRealm <shafaghi1378@gmail.com>
2020-02-17 07:14:51 -0500

    Changin the order of fields in gdt_entry_struct

--- src/descriptor_tables.h
+++ src/descriptor_tables.h
@@ -19,20 +19,19 @@ void init_descripotor_table();
 
 struct gdt_entry_struct
 {
-    u16int limit_low;
     u16int base_low;
+    u16int limit_low;
     u8int  base_middle;
-    u8int  base_high;
     u8int  access;
-    u8int granularity;
+    u8int  granularity;
+    u8int  base_high;
 } __attribute__((packed));
-
 typedef struct gdt_entry_struct gdt_entry_t;
 
 struct gdt_ptr_struct
 {
-    u16int base;
     ubaseint limit;
+    u16int   base;
 } __attribute__((packed));
-
 typedef struct gdt_ptr_struct gdt_ptr_t;
+
