f5ad447618d78b81cbe8b261010cc9bc3ea397b3
NothingRealm <shafaghi1378@gmail.com>
2020-02-28 14:03:15 -0500

    A little bit of doc for GDT

--- src/descriptor_tables.h
+++ src/descriptor_tables.h
@@ -1,11 +1,21 @@
 
 // This file contain GDT and IDT prototypes
 
+/* 
+ * -------   limit    -------
+ * limit is made of 20 bits 16 bits. (lsb) are in limit_low and 4 are in 
+ * granularity lsb
+ * --------------------------
+ *
+ * -------    base    -------
+ * base is made of 32 bits. in three parts. low part is 16 bits and others are
+ * 8 each.
+ * --------------------------
+ */
 
 #include "common.h"
 
 void init_descripotor_table();
-
 
 struct gdt_entry_struct
 {
