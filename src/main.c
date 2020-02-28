92fd9edf8a3a6a4029b9dad751f55121cb60c83c
NothingRealm <shafaghi1378@gmail.com>
2020-02-28 03:20:38 -0500

    Some function call for checking if monitor works

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 //           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
 
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
-    // All our initialisation calls will go in here.
-    return 0xDEADBABA;
+
+    monitor_clear();
+
+    monitor_puts("Hello, World");
+
+    return 0;
 }
